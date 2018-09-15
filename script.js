a27c8fcc710122dbf2f13e01922a9b7c55841dbd
Victor <victor-lf@hotmail.com>
2018-09-15 06:27:41 +0000

    Add script for search field shadow

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("search").setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.08)");
+}
+
+function myFunction2() {
+    document.getElementById("search").setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08)");
+}
