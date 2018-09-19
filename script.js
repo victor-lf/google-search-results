2504ef6fec2504cfddc6320e1984b705290149e8
Victor <victor-lf@hotmail.com>
2018-09-19 04:06:57 +0000

    Remove onfocus and onblur attributes and use addEventListener method instead to change the search field shadow

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function myFunction() {
-    document.getElementById("search").setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.08);");
-}
+var form = document.getElementsByTagName("form")[0];
+var input = document.getElementsByTagName("input")[0];
 
-function myFunction2() {
-    document.getElementById("search").setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);");
-}
+input.addEventListener("focus", function() {
+    form.setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.08);");
+});
+
+input.addEventListener("blur", function() {
+    form.setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);");
+})
