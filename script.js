e5dc299197987fb88848048cab29720c636bd1bd
Victor <victor-lf@hotmail.com>
2018-09-24 15:10:29 +0000

    Use named functions instead of anonymous functions

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var input = document.getElementsByTagName("input")[0];
-var form = input.parentNode
+var form = input.parentNode;
 
-input.addEventListener("focus", function() {
-    form.setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.08);");
-});
+input.addEventListener("focus", focus);
+input.addEventListener("blur", blur);
 
-input.addEventListener("blur", function() {
-    form.setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);");
-})
+function focus() {
+    form.setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2)," +
+                                           "0 0 0 1px rgba(0, 0, 0, 0.08);")
+}
+
+function blur() {
+    form.setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16)," +
+                                           "0 0 0 1px rgba(0, 0, 0, 0.08);")
+}
