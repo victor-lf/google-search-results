320a74468942a47f9cb03ac903252d0974c0a258
Victor <victor-lf@hotmail.com>
2018-09-27 15:11:25 +0000

    Use single quotes instead of double quotes because it's easier to type

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /* jshint esversion: 6, strict: true */
 
-let input = document.querySelector("input");
+let input = document.querySelector('input');
 let form = input.parentNode;
 
-input.addEventListener("focus", focus);
-input.addEventListener("blur", blur);
+input.addEventListener('focus', focus);
+input.addEventListener('blur', blur);
 
 function focus() {
-    "use strict";
-    form.setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2)," +
-            "0 0 0 1px rgba(0, 0, 0, 0.08);");
+    'use strict';
+    form.setAttribute('style', 'box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2),' +
+            '0 0 0 1px rgba(0, 0, 0, 0.08);');
 }
 
 function blur() {
-    "use strict";
-    form.setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16)," +
-            "0 0 0 1px rgba(0, 0, 0, 0.08);");
+    'use strict';
+    form.setAttribute('style', 'box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16),' +
+            '0 0 0 1px rgba(0, 0, 0, 0.08);');
 }
