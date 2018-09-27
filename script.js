60effa830b886cef9922787f476c89699f0d504e
Victor <victor-lf@hotmail.com>
2018-09-27 02:29:56 +0000

    Some linting with jshint

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6, strict: true */
+
 let input = document.getElementsByTagName("input")[0];
 let form = input.parentNode;
 
@@ -5,11 +7,13 @@ input.addEventListener("focus", focus);
 input.addEventListener("blur", blur);
 
 function focus() {
+    "use strict";
     form.setAttribute("style", "box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2)," +
-                                           "0 0 0 1px rgba(0, 0, 0, 0.08);")
+            "0 0 0 1px rgba(0, 0, 0, 0.08);");
 }
 
 function blur() {
+    "use strict";
     form.setAttribute("style", "box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16)," +
-                                           "0 0 0 1px rgba(0, 0, 0, 0.08);")
+            "0 0 0 1px rgba(0, 0, 0, 0.08);");
 }
